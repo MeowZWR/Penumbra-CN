27e71ee1e3a2756d8b476ccc35b108bf128fe7a3
Tsubasahane <wozaiha@gmail.com>
2025-10-09 15:47:52 +0800

    Dont show players res using mare

--- Penumbra/UI/AdvancedWindow/ResourceTreeViewer.cs
+++ Penumbra/UI/AdvancedWindow/ResourceTreeViewer.cs
@@ -97,6 +97,12 @@ public class ResourceTreeViewer(
                         continue;
                 }
 
+                if (tree.CollectionName.Contains("Mare", StringComparison.OrdinalIgnoreCase))
+                {
+                    ImUtf8.TextFrameAligned("该角色数据由Mare创建和管理, 如需修改请关闭后再试...");
+                    continue;
+                }
+
                 using var id = ImRaii.PushId(index);
 
                 ImUtf8.TextFrameAligned($"合集：{(incognito.IncognitoMode ? tree.AnonymizedCollectionName : tree.CollectionName)}");
